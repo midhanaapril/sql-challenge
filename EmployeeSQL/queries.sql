3a0d99a31c3d00c002014c66ed0d76459ad81b02
HannahK <89614469+midhanaapril@users.noreply.github.com>
2021-11-13 01:25:19 -0500

    Create queries.sql

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,39 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM salaries AS s
+INNER JOIN employees AS e ON e.emp_no = s.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date 
+FROM employees 
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+SELECT d.dept_no, dept_name, employees.emp_no, first_name, last_name 
+FROM employees
+INNER JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+INNER JOIN departments AS d ON dept_manager.dept_no = d.dept_no;
+
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+SELECT employees.emp_no, last_name, first_name, dept_name
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments AS d ON dept_emp.dept_no = d.dept_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, including their 
+--employee number, last name, first name, and department name.
+SELECT employees.emp_no, first_name, last_name, dept_name 
+FROM employees 
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_no IN ('d007', 'd005');
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
